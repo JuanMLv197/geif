61e7881ed76890237c461e721a9e8762e94e882e
alumno <alumno@gmail.com>
2025-02-10 16:51:18 +0100

    Com_4

--- lib/attr.sh
+++ lib/attr.sh
@@ -6,10 +6,31 @@ attr_int_list(){
 
 }
 
-attr_int_ip(){
+attr_int_state(){
 
 	int="$1"
-	ip addr show dev $int | grep "inet " | cut -f6 -d" " | cut -f1 -d/	
+	ip -c=never addr show dev enp0s3 | grep -o "state.*" | cut -f2 -d" "
+	unset $int
 
 }
 
+attr_int_ip(){
+
+	int="$1"	
+	ip -c=never addr show dev enp0s3 | grep -o "inet .*" | cut -f2 -d" "
+	unset $int
+
+}
+
+attr_int_mac(){
+
+	int="$1"
+	ip -c=never link show dev enp0s3 | grep -o "link.*" | cut -f2 -d" "
+	unset $int
+
+}
+
+attr_int_list
+attr_int_state enp0s3
+attr_int_ip enp0s3
+attr_int_mac enp0s3
